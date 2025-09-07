9d0ef9a497532e6dc1b2927d6ce5c28db871cda5
yokabalaji <108510493+yokabalaji@users.noreply.github.com>
2025-09-07 21:17:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
+const purchaseRoutes = require('./routes/purchaseRoutes')
+const pool = require('./config/db') // import db
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+
+// Routes
+app.use('/auth', authRoutes)
+app.use('/users', userRoutes)
+app.use('/purchases', purchaseRoutes)
+
+pool
+  .connect()
+  .then(client => {
+    console.log('✅ Database connected successfully')
+    client.release()
+  })
+  .catch(err => {
+    console.error('❌ Database connection error:', err.message)
+  })
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
