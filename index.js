60ca6077f3b613a27bf99d496526ba16fdc602bb
yokabalaji <108510493+yokabalaji@users.noreply.github.com>
2025-09-08 06:45:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv')
 const authRoutes = require('./routes/authRoutes')
 const userRoutes = require('./routes/userRoutes')
 const purchaseRoutes = require('./routes/purchaseRoutes')
-const pool = require('./config/db') // import db
+const pool = require('./config/db')
 
 dotenv.config()
 const app = express()
@@ -27,4 +27,5 @@ pool
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
 
