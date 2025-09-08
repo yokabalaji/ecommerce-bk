9160c6e58d38b59193fa7cef4965225a274bfaff
yokabalaji <108510493+yokabalaji@users.noreply.github.com>
2025-09-08 06:44:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 
-// Routes
 app.use('/auth', authRoutes)
 app.use('/users', userRoutes)
 app.use('/purchases', purchaseRoutes)
@@ -18,13 +17,14 @@ app.use('/purchases', purchaseRoutes)
 pool
   .connect()
   .then(client => {
-    console.log('✅ Database connected successfully')
+    console.log('Database connected successfully')
     client.release()
   })
   .catch(err => {
-    console.error('❌ Database connection error:', err.message)
+    console.error('Database connection error:', err.message)
   })
 
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
